bba499a8a45d67bdae3f087ffea00611eff47895
cosmo-0910 <christianahadetomi@gmail.com>
2026-03-04 09:34:59 +0100

    Fix: Increase Workbox maximumFileSizeToCacheInBytes to 4MB

--- vite.config.ts
+++ vite.config.ts
@@ -26,6 +26,9 @@ export default defineConfig({
             type: 'image/png'
           }
         ]
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 4000000 // 4MB
       }
     })
   ],
